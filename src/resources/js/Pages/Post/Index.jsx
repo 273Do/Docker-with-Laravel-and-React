a01fa704a54f9f2d46c2ab7b28dfc7e782141934
273* <keita.y310@icloud.com>
2024-10-30 17:29:44 +0900

    ✨ 削除処理の実装

--- src/resources/js/Pages/Post/Index.jsx
+++ src/resources/js/Pages/Post/Index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
-import { Link } from "@inertiajs/react";
+import { Link, router } from "@inertiajs/react";
 
 const Index = (props) => {
     const { posts } = props;
-    console.log(props);
+
+    const handleDeletePost = (id) => {
+        router.delete(`/posts/${id}`, {
+            onBefore: () => confirm("本当に削除しますか？"),
+        });
+    };
 
     return (
         <Authenticated
@@ -16,7 +21,8 @@ const Index = (props) => {
             }
         >
             <div className="p-12">
-                <Link href="/posts/create">Create</Link>
+                <Link href="/posts/create">create</Link>
+
                 <h1>Blog Name</h1>
 
                 {posts.map((post) => (
@@ -24,8 +30,14 @@ const Index = (props) => {
                         <h2>
                             <Link href={`/posts/${post.id}`}>{post.title}</Link>
                         </h2>
-
                         <p>{post.body}</p>
+
+                        <button
+                            className="p-1 bg-purple-300 hover:bg-purple-400 rounded-md"
+                            onClick={() => handleDeletePost(post.id)}
+                        >
+                            delete
+                        </button>
                     </div>
                 ))}
             </div>
